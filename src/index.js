93043779c82348c47450c091e56caaa7c99fed4b
Zin Min Aung <162323461+Zinminsky@users.noreply.github.com>
2026-01-08 15:10:48 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Set CSS variables for public URL images
+const rootElement = document.documentElement;
+rootElement.style.setProperty('--bg-image', `url(${process.env.PUBLIC_URL || ''}/images/background-image.png)`);
+rootElement.style.setProperty('--section-image-2', `url(${process.env.PUBLIC_URL || ''}/images/section-image-2.svg)`);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
